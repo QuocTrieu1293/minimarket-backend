23cbd4eaffd9b4e8ac85d2a6639db500922848f9
QuocTrieu <21522714@gm.uit.edu.vn>
2024-05-20 01:34:52 +0700

    azure blob storage #2

---
+++ app/Providers/AzureServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Filesystem;
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use League\Flysystem\AzureBlobStorage\AzureBlobStorageAdapter;
+
+
+class AzureServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Storage::extend('azure', function ($app, $config) {
+            $connectionString = "DefaultEndpointsProtocol=https;AccountName=" . $config['name'] . ";AccountKey=" . $config['key'] . ";EndpointSuffix=core.windows.net";
+            $client = BlobRestProxy::createBlobService($connectionString);
+
+            return new Filesystem(new AzureBlobStorageAdapter($client, $config['container']));
+        });
+    }
+}
